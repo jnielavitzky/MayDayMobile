c8cf68972de065ade255c8bd33131d7007866786
Ioni Nielavitzky <ioninielavitzky@MacBook-Pro-de-Jonatan.local>
2017-06-24 19:10:38 -0300

    Tickets con data dinamica!

--- app/src/main/java/jnielavitzky/itba/com/maydaymobile/MisVuelosActivity.java
+++ app/src/main/java/jnielavitzky/itba/com/maydaymobile/MisVuelosActivity.java
@@ -1,22 +1,18 @@
 package jnielavitzky.itba.com.maydaymobile;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 
-import com.dexafree.materialList.card.Card;
-import com.dexafree.materialList.card.CardProvider;
-import com.dexafree.materialList.card.OnActionClickListener;
-import com.dexafree.materialList.card.action.TextViewAction;
-import com.dexafree.materialList.view.MaterialListView;
-
-import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -26,7 +22,7 @@ import org.json.JSONObject;
 public class MisVuelosActivity extends Fragment {
 
 
-    MaterialListView cardList;
+
 
     public MisVuelosActivity() {
     }
@@ -57,43 +53,94 @@ public class MisVuelosActivity extends Fragment {
         View view = inflater.inflate(R.layout.mis_vuelos_fragment, container, false);
 
 
-
-
-
-
-
         // Inflate the layout for this fragment
         ((MainActivity) getActivity()).setActionBarTitle(getResources().getString(R.string.mis_vuelos));
 
 
-        cardList = (MaterialListView)view.findViewById(R.id.material_listview);
+        TableLayout cardList = (TableLayout)view.findViewById(R.id.material_listview);
 
+        //Sample data
+        JSONObject data = new JSONObject();
+        try {
+            data.put("desde", "ARG");
+            data.put("hasta", "MIA");
+            data.put("estado", "Aterrizado");
+            data.put("info_from", "Silvio Pettirossi • Lun, May 29 ");
+            data.put("salida_time_from", "12:34 AM");
+            data.put("terminal_num_from", "K2");
+            data.put("gate_num_from", "Q12");
+            data.put("despegue_program_time", "22:23 AM");
 
+            data.put("info_to", "Ministro Pistarini • Lun, May 29 ");
+            data.put("salida_time_to", "22:34 AM");
+            data.put("terminal_num_to", "A12");
+            data.put("gate_num_to", "B234");
+            data.put("aterrizaje_program_time", "02:33 AM");
 
-        int white = Color.WHITE;
-
-
-        for (int i = 0; i < 10; i++) {
-            Card card = new Card.Builder(view.getContext())
-                    .setTag("BASIC_IMAGE_BUTTONS_CARD")
-                    .withProvider(new CardProvider())
-                    .setLayout(R.layout.test)
-//                    .setTitle("Buenos Aires → Asuncion")
-                    .setTitleColor(white)
-                    .setDrawable(R.drawable.test)
-
-                    .endConfig()
-                    .build();
-
-            cardList.getAdapter().add(card);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
+        for (int i = 0; i < 10; i++) {
+            //Inflador
+            LayoutInflater infl = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout layout = (LinearLayout) infl.inflate(R.layout.card_template, null);
 
+            try {
+                fillData(layout, data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
+            cardList.addView(layout);
 
+        }
         return view;
     }
 
+
+    private void fillData(LinearLayout card, JSONObject data) throws JSONException {
+
+        TextView desde = (TextView)card.findViewById(R.id.from);
+        desde.setText(data.getString("desde"));
+
+        TextView hasta = (TextView)card.findViewById(R.id.to);
+        hasta.setText(data.getString("hasta"));
+
+        TextView estado = (TextView)card.findViewById(R.id.estado);
+        estado.setText(data.getString("estado"));
+
+        TextView info_from = (TextView)card.findViewById(R.id.info_from);
+        info_from.setText(data.getString("info_from"));
+
+        TextView salida_time_from = (TextView)card.findViewById(R.id.salida_time_from);
+        salida_time_from.setText(data.getString("salida_time_from"));
+
+        TextView terminal_num_from = (TextView)card.findViewById(R.id.terminal_num_from);
+        terminal_num_from.setText(data.getString("terminal_num_from"));
+
+        TextView gate_num_from = (TextView)card.findViewById(R.id.gate_num_from);
+        gate_num_from.setText(data.getString("gate_num_from"));
+
+        TextView despegue_program_time = (TextView)card.findViewById(R.id.despegue_program_time);
+        despegue_program_time.setText(data.getString("despegue_program_time"));
+
+
+        TextView info_to = (TextView)card.findViewById(R.id.info_to);
+        info_to.setText(data.getString("info_to"));
+
+        TextView salida_time_to = (TextView)card.findViewById(R.id.salida_time_to);
+        salida_time_to.setText(data.getString("salida_time_to"));
+
+        TextView terminal_num_to = (TextView)card.findViewById(R.id.terminal_num_to);
+        terminal_num_to.setText(data.getString("terminal_num_to"));
+
+        TextView gate_num_to = (TextView)card.findViewById(R.id.gate_num_to);
+        gate_num_to.setText(data.getString("gate_num_to"));
+
+        TextView aterrizaje_program_time = (TextView)card.findViewById(R.id.aterrizaje_program_time);
+        aterrizaje_program_time.setText(data.getString("aterrizaje_program_time"));
+    }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
