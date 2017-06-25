a477baf32478e596d4c49f7fce3d45024966b1a7
Martín Capparelli <mcapparelli@itba.edu.ar>
2017-06-25 16:07:46 -0300

    para marti

--- app/src/main/java/jnielavitzky/itba/com/maydaymobile/MainActivity.java
+++ app/src/main/java/jnielavitzky/itba/com/maydaymobile/MainActivity.java
@@ -51,8 +51,6 @@ public class MainActivity extends AppCompatActivity
         //API
         new API().execute();
 
-        Notifications.createNotification();
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
